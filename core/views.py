da773af9173320b906aea19db7eef6968d8d5c45
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-12-30 22:22:46 -0300

    Create Product ModelViewSet

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import ProductSerializer
+from .models import Product
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()

27cc6793d85e050c04203c9e89d0c5858f3061a2
Garibaldi Violin Neto <garibaldi.neto@cargobr.com>
2019-12-30 22:21:42 -0300

    Create products url

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from .views import ProductViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = router.urls
